2df28bb69496bd7058819f4333f6a00cc6042342
DanishAR12 <160243905+DanishAR12@users.noreply.github.com>
2025-12-27 21:09:11 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var fruits = ["apple","banana","orange","melon"];
+console.log(fruits);
+
+// fruits.splice(fruits.length,0,"gava");  //   push method by splice
+//  console.log(fruits);
+
+// fruits.splice(-1,1);
+// console.log(fruits);   //  pop method by splice
+
+// fruits.splice(0,0,"grapes","mango");   //  unshift method by splice
+// console.log(fruits);
+
+fruits.splice(0,1,);
+console.log(fruits);     // shift method by splice
